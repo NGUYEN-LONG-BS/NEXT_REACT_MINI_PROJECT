905a97ee92b3ce3fcd73926e640fb54a407171b6
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2025-03-12 14:28:05 +0700

    commit

---
+++ my_next_app_num_01/src/data/products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+      id: 1,
+      name: "Hoa Hồng Đỏ",
+      description: "Đẹp và lãng mạn, hoa hồng đỏ là biểu tượng của tình yêu.",
+      price: 300000,
+      image: "/images/rose.jpg"
+    },
+    {
+      id: 2,
+      name: "Hoa Lan Hồ Điệp",
+      description: "Làn sóng tinh tế và sang trọng của hoa lan hồ điệp.",
+      price: 500000,
+      image: "/images/orchid.jpg"
+    },
+    // Thêm sản phẩm khác vào đây
+  ];
+  
+  export default products;
